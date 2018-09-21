e26100997576fc5c17981035c62c02b3965187a8
kinegratii <kinegratii@gmail.com>
2018-09-21 11:41:19 +0800

    :sparkles: new module borax.ui.aiotk

---
+++ borax/ui/aiotk.py
@@ -0,0 +1,16 @@
+# coding=utf8
+
+import tkinter as tk
+import asyncio
+
+__all__ = ['run_loop']
+
+
+async def run_loop(self, interval=0.05):
+    try:
+        while True:
+            self.update()
+            await asyncio.sleep(interval)
+    except tk.TclError as e:
+        if "application has been destroyed" not in e.args[0]:
+            raise
