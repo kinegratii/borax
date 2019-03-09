92d7b5635cc38c4f3a761a869c0c765ba323e839
kinegratii <kinegratii@gmail.com>
2019-03-09 19:29:34 +0800

    :art: add test cases

---
+++ tests/test_proxy.py
@@ -0,0 +1,23 @@
+# coding=utf8
+
+import unittest
+
+from borax.patterns.proxy import Proxy
+
+
+class MockPoint(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_distance(self):
+        return self.x ** 2 + self.y ** 2
+
+
+class ProxyTestCase(unittest.TestCase):
+    def test_proxy(self):
+        proxy = Proxy()
+        with self.assertRaises(AttributeError):
+            proxy.get_distance()
+        proxy.initialize(MockPoint(3, 4))
+        self.assertEqual(25, proxy.get_distance())
