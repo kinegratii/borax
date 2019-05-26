82db18584d4f493d852aeda72ab66c059894409d
kinegratii <kinegratii@gmail.com>
2019-05-26 20:54:32 +0800

    :sparkles: add rotate_filename function

--- tests/test_string_convert.py
+++ tests/test_string_convert.py
@@ -1,8 +1,12 @@
 # coding=utf8
 
+from datetime import datetime
+
 import unittest
+from unittest.mock import Mock, patch
 
 from borax.strings import camel2snake, snake2camel, get_percentage_display
+from borax.system import rotate_filename
 
 FIXTURES = [
     ('HelloWord', 'hello_word'),
@@ -25,3 +29,58 @@ class PercentageStringTestCase(unittest.TestCase):
     def test_convert(self):
         self.assertEqual('100.00%', get_percentage_display(1, places=2))
         self.assertEqual('56.23%', get_percentage_display(0.5623))
+
+
+class FilenameRotateTestCase(unittest.TestCase):
+    @patch('borax.system.datetime')
+    def test_rotate(self, my_datetime):
+        my_datetime.now = Mock(return_value=datetime(2019, 5, 25))
+        self.assertEqual(
+            'demo_20190525.docx',
+            rotate_filename('demo.docx', time_fmt='%Y%m%d')
+        )
+        self.assertEqual(
+            'demo-2019_05_25.docx',
+            rotate_filename('demo.docx', time_fmt='%Y_%m_%d', sep='-')
+        )
+
+        self.assertEqual(
+            'demo-2019_05_25.docx',
+            rotate_filename('demo.docx', time_fmt='%Y_%m_%d', sep='-')
+        )
+        # Full path
+        self.assertEqual(
+            '/usr/home/pi/demo-2019_05_25.docx',
+            rotate_filename('/usr/home/pi/demo.docx', time_fmt='%Y_%m_%d', sep='-')
+        )
+        # name with a dot char.
+        self.assertEqual(
+            '/usr/home/pi/bws-v3.2.1-upgrade_2019_05_25.sql',
+            rotate_filename('/usr/home/pi/bws-v3.2.1-upgrade.sql', time_fmt='%Y_%m_%d', sep='_')
+        )
+
+    def test_with_one_datetime(self):
+        now = datetime(2019, 5, 23)
+        self.assertEqual(
+            'demo_20190523.docx',
+            rotate_filename('demo.docx', time_fmt='%Y%m%d', now=now)
+        )
+        self.assertEqual(
+            'demo-2019_05_23.docx',
+            rotate_filename('demo.docx', time_fmt='%Y_%m_%d', sep='-', now=now)
+        )
+
+        self.assertEqual(
+            'demo-2019_05_23.docx',
+            rotate_filename('demo.docx', time_fmt='%Y_%m_%d', sep='-', now=now)
+        )
+        # Full path
+        self.assertEqual(
+            '/usr/home/pi/demo-2019_05_23.docx',
+            rotate_filename('/usr/home/pi/demo.docx', time_fmt='%Y_%m_%d', sep='-', now=now)
+        )
+        # name with a dot char.
+        self.assertEqual(
+            '/usr/home/pi/bws-v3.2.1-upgrade_2019_05_23.sql',
+            rotate_filename('/usr/home/pi/bws-v3.2.1-upgrade.sql', time_fmt='%Y_%m_%d', sep='_', now=now)
+        )
