86681269fe7365425ddb101ad9802981eb262348
kinegratii <kinegratii@gmail.com>
2020-08-10 21:03:25 +0800

    :sparkles: Make financial_amount_capital deprecated

--- borax/finance.py
+++ borax/finance.py
@@ -1,7 +1,20 @@
 # coding=utf8
 
+from decimal import Decimal
+
+from typing import Union
+
+import warnings
+
 from borax.numbers import FinanceNumbers
 
 __all__ = ['financial_amount_capital']
 
-financial_amount_capital = FinanceNumbers.to_capital_str
+
+def financial_amount_capital(num: Union[int, float, Decimal, str]) -> str:
+    warnings.warn(
+        'This method is deprecated and will be removed in V3.5.Use borax.numbers.FinanceNumbers instead.',
+        category=PendingDeprecationWarning
+    )
+
+    return FinanceNumbers.to_capital_str(num)
