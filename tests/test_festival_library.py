ed8eb3aded47b296edea6304d6d6bc9b7b9733a5
kinegratii <kinegratii@gmail.com>
2025-03-16 19:35:36 +0800

    :bug: Fix test error for FestivalLibrary.load

--- tests/test_festival_library.py
+++ tests/test_festival_library.py
@@ -27,8 +27,6 @@ class FestivalLibraryTestCase(unittest.TestCase):
     def test_new_load(self):
         fl = FestivalLibrary.load('basic')
         self.assertEqual(33, len(fl))
-        fl2 = FestivalLibrary.load(r'tests\test_festivals_datasets.csv')
-        self.assertEqual(33, len(fl2))
         with self.assertRaises(FestivalDatasetNotExist):
             FestivalLibrary.load('not-found')
 
